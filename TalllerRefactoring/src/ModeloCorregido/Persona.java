86f05b68d8fea76feb89e07b92ebe9d6883be88a
AToala <aitoala@espol.edu.ec>
2020-12-23 17:41:28 -0500

    Duplicate Code - Estudiante y Profesor

---
+++ TalllerRefactoring/src/ModeloCorregido/Persona.java
@@ -0,0 +1,70 @@
+package ModeloCorregido;
+
+import java.util.ArrayList;
+
+public abstract class Persona{
+	public String nombre;
+    public String apellido;
+    public int edad;
+    public String direccion;
+    public String telefono;
+    public String facultad;
+    public ArrayList<Paralelo> paralelos;
+
+    public Persona(String nombre, String apellido, int edad, String direccion, String telefono){
+    	this.nombre = nombre;
+    	this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        paralelos= new ArrayList<>();
+    }
+
+    //Getter y setter del Nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //Getter y setter del Apellido
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+    //Getter y setter de la edad
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    //Getter y setter de la direccion
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    //Getter y setter del telefono
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
--- TalllerRefactoring/src/ModeloCorregido/Profesor.java
+++ TalllerRefactoring/src/ModeloCorregido/Profesor.java
@@ -1,7 +1,22 @@
 package ModeloCorregido;
 
+import java.util.ArrayList;
+
 public class Profesor {
+    public String codigo;
+    public int añosdeTrabajo;
+    public double bonoFijo;
 
-	
+    public Profesor(String nombre, String apellido, int edad, String direccion, String telefono, String cod, int añosdeTrabajo, double bonofijo) {
+        super(nombre, apellido, edad, direccion, telefono);
+        this.codigo = cod;
+        this.añosdeTrabajo = añosdeTrabajo;
+        this.bonoFijo = bonofijo
+
+    }
+    
+    public void anadirParalelos(Paralelo p){
+        paralelos.add(p);
+    }
+
 }
-
