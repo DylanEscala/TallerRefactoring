9bb8ccd906a17255a87df46050ad49953eb1cfe1
Melina Macias <melina_maria@hotmail.es>
2020-12-23 16:42:18 -0500

    Refactoring Code Smell Long Parameter List

--- TalllerRefactoring/src/ModeloCorregido/Estudiante.java
+++ TalllerRefactoring/src/ModeloCorregido/Estudiante.java
@@ -81,18 +81,19 @@ public class Estudiante {
     }
     
     //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    public double CalcularNota(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
+    public double CalcularNota(Paralelo p,Nota notas){
         double nota=0;
         for(Paralelo par:paralelos){
             if(p.equals(par)){
-                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
-                double notaPractico=(ntalleres)*0.20;
+                double notaTeorico=(notas.getNexamen()+notas.getNdeberes()+notas.getNlecciones())*0.80;
+                double notaPractico=(notas.getNtalleres())*0.20;
                 nota=notaTeorico+notaPractico;
             }
         }
         return nota;
     }
-        
+     
+    
     //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
     public double CalcularNotaTotal(Paralelo p){
         double notaTotal=0;
