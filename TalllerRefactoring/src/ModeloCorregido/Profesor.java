a9695b66eee484baf1462dc63b77224c1c5ea8ec
Joel Alvarado <joealalv@espol.edu.ec>
2020-12-23 17:02:11 -0500

    Collapse Hierarchy between InfoAdicionalProfesor and Profesor

--- TalllerRefactoring/src/ModeloCorregido/Profesor.java
+++ TalllerRefactoring/src/ModeloCorregido/Profesor.java
@@ -1,7 +1,37 @@
 package ModeloCorregido;
 
-public class Profesor {
+import java.util.ArrayList;
 
+import modelos.InformacionAdicionalProfesor;
+import modelos.Paralelo;
+
+public class Profesor {
+	public String codigo;
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String direccion;
+    public String telefono;
+    public InformacionAdicionalProfesor info;
+    public ArrayList<Paralelo> paralelos;
+    public int aniosdeTrabajo;
+    public String facultad;
+    public double BonoFijo;
+    
+
+    public Profesor(String codigo, String nombre, String apellido, String facultad, int edad, String direccion, String telefono) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        paralelos= new ArrayList<>();
+    }
+    
+    public void anadirParalelos(Paralelo p){
+        paralelos.add(p);
+    }
 	
 }
 
