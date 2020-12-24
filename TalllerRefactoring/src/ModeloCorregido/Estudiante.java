6681005cf5e0c17df153d6fd3bb800eb1d6b4127
Alexandra Ramos <maraplua@espol.edu.ec>
2020-12-23 21:30:59 -0500

    Comments - Comentarios innecesarios

--- TalllerRefactoring/src/ModeloCorregido/Estudiante.java
+++ TalllerRefactoring/src/ModeloCorregido/Estudiante.java
@@ -4,50 +4,36 @@ import java.util.ArrayList;
 
 import modelos.Paralelo;
 
-public class Estudiante extends Persona{
-	 //Informacion del estudiante
+public class Estudiante extends Persona {
+
     public String matricula;
-    
+
     public Estudiante(String nombre, String apellido, int edad, String direccion, String telefono, String facultad, String matricula) {
         super(nombre, apellido, edad, direccion, telefono, facultad);
         this.matricula = matricula;
     }
-    
-    
-    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    public double CalcularNota(Paralelo p,Nota notas){
-        double nota=0;
-        for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                double notaTeorico=(notas.getNexamen()+notas.getNdeberes()+notas.getNlecciones())*0.80;
-                double notaPractico=(notas.getNtalleres())*0.20;
-                nota=notaTeorico+notaPractico;
+
+    public double CalcularNota(Paralelo p, Nota notas) {
+        double nota = 0;
+        for (Paralelo par : paralelos) {
+            if (p.equals(par)) {
+                double notaTeorico = (notas.getNexamen() + notas.getNdeberes() + notas.getNlecciones()) * 0.80;
+                double notaPractico = (notas.getNtalleres()) * 0.20;
+                nota = notaTeorico + notaPractico;
             }
         }
         return nota;
     }
-     
-    
-    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
-    public double CalcularNotaTotal(Paralelo p){
-        double notaTotal=0;
-        for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                notaTotal=(p.getMateria().notaInicial+p.getMateria().notaFinal)/2;
-                
+
+    public double CalcularNotaTotal(Paralelo p) {
+        double notaTotal = 0;
+        for (Paralelo par : paralelos) {
+            if (p.equals(par)) {
+                notaTotal = (p.getMateria().notaInicial + p.getMateria().notaFinal) / 2;
+
             }
         }
         return notaTotal;
-        
+
     }
 }
-        
-    
-    
-    
-            
-        
-        
-
-	
-
