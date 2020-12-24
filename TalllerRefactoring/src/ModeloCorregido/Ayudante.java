f3896c241164fb3b75abe0915b03c091719b5519
DylanEscala <descala@espol.edu.ec>
2020-12-23 20:57:31 -0500

    fixing my mistake

--- TalllerRefactoring/src/ModeloCorregido/Ayudante.java
+++ TalllerRefactoring/src/ModeloCorregido/Ayudante.java
@@ -2,20 +2,17 @@ package ModeloCorregido;
 
 import java.util.ArrayList;
 
-import ModeloCorregido.Paralelo;
 
-public class Ayudante extends Estudiante{
+public class Ayudante extends ModeloCorregido.Estudiante{
     public ArrayList<Paralelo> paralelos;
 
     Ayudante(String nombre, String apellido, int edad, String direccion, String telefono, String facultad, String matricula){
-    	super( nombre,  apellido,  edad,  direccion,  telefono,  facultad,  matricula);
+    	super(nombre, apellido, edad, direccion, telefono, facultad,matricula);
     }
-    
+   
     //Getters y setters se delegan en objeto estudiante para no duplicar código
-    
-
+   
     //Los paralelos se añaden/eliminan directamente del Arraylist de paralelos
-
 
     //Método para imprimir los paralelos que tiene asignados como ayudante
     public void MostrarParalelos(){
--- TalllerRefactoring/src/modelos/Ayudante.java
+++ TalllerRefactoring/src/modelos/Ayudante.java
@@ -2,16 +2,32 @@ package modelos;
 
 import java.util.ArrayList;
 
-public class Ayudante extends ModeloCorregido.Estudiante{
+public class Ayudante {
+    protected Estudiante est;
     public ArrayList<Paralelo> paralelos;
 
-    Ayudante(String nombre, String apellido, int edad, String direccion, String telefono, String facultad, String matricula){
-    	super(nombre, apellido, edad, direccion, telefono, facultad,matricula);
+    Ayudante(Estudiante e){
+    	est = e;
     }
-   
+    public String getMatricula() {
+        return est.getMatricula();
+    }
+
+    public void setMatricula(String matricula) {
+        est.setMatricula(matricula);
+    }
+
     //Getters y setters se delegan en objeto estudiante para no duplicar código
-   
+    public String getNombre() {
+        return est.getNombre();
+    }
+
+    public String getApellido() {
+        return est.getApellido();
+    }
+
     //Los paralelos se añaden/eliminan directamente del Arraylist de paralelos
+
 
     //Método para imprimir los paralelos que tiene asignados como ayudante
     public void MostrarParalelos(){
